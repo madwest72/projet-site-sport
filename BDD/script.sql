dd4cdc9ec9f9b1e1a901308390b27eac58ac8c12
Madwest <mamadou.abdiallo@outlook.fr>
2026-01-25 09:54:13 +0100

    sauvegarde

---
+++ BDD/script.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE mm;
+
+use mm
+
+CREATE TABLE utilisateur (
+    
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    nom VARCHAR(255),
+    prenom VARCHAR(255),
+    email VARCHAR(255),
+    mdp VARCHAR(255) NOT NULL,
+    date_naissance DATE,
+    adresse VARCHAR(255),
+    tel VARCHAR(10),
+    sport VARCHAR(255),
+    professionnel BOOLEAN,
+    admins BOOLEAN
+);
+
+CREATE TABLE sport (
+    
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    foot BOOLEAN,
+    basket BOOLEAN,
+    athle BOOLEAN,
+    natation BOOLEAN,
+    danse BOOLEAN
+
+);
