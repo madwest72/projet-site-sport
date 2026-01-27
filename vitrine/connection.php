031402de4f69f88b21a0fda40eb6e49f28124e28
matthieuprudentg-glitch <matthieu.prudent.g@gmail.com>
2026-01-27 14:53:11 +0100

    Add login form and site description to connection.php

--- vitrine/connection.php
+++ vitrine/connection.php
@@ -1,6 +1,52 @@
 <?php include('commun2/header1.php'); ?>
 
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-mg-1 col-lg-12">
 
+                <div class="card shadow-lg border-0" style="border-top: 5px solid #ff6600; border-radius: 15px; overflow: hidden;">
+                    <div class="row g-0">
 
+                        <div class="col-lg-6 p-4">
+                            <div class="text-center mb-3">
+                                <h6 class="text-uppercase fw-bold mb-4" style="color: #ff6600; letter-spacing: 1px;">Inscriptions Sport</h6>
+                            </div>
+
+                            <form>
+                                <div class="text-center mb-3">
+                                    <p>Rentrez vos identifiants</p>
+                                </div>
+
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <label class="form-label" for="idautilisierplustardonverra">Identifiant</label>
+                                    <input type="email" id="idautilisierplustardonverra" class="form-control" placeholder="XxxID123xxX" />
+                                </div>
+
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <label class="form-label" for="motdepasse">Password</label>
+                                    <input type="password" id="motdepasse" class="form-control" />
+                                </div>
+
+                                <div class="text-center pt-1 mb-5 pb-1">
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-block fa-lg mb-3 text-white" style="background-color: #ff6600;" type="button">Log in</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="col-lg-6 d-flex align-items-center" style="background-color: #ff7700;">
+                            <div class="text-black px-4 py-4 p-md-5 mx-md-4">
+                                <h4 class="mb-4">À propos du site</h4>
+                                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 <?php include('commun2/footer1.php'); ?>
